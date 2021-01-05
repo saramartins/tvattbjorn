83e55fc35a3a3a07a2ba7c252536d3ec04ed2c98
Sara Martin <sara.elisabet.martin@gmail.com>
2021-01-05 23:37:09 +0100

    Basic structure

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import { StyleSheet, Text, View, Footer } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Footer style={styles.footer}>Test</Footer>
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
@@ -12,8 +14,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f9f9f9',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  footer: {
+    backgroundColor: '#000',
+  }
 });
